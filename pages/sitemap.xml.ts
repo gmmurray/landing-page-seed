1da66547b03fe15e8acf5c8a384aca9ae95bd23b
gmmurray <trinilime123@live.com>
2023-09-04 18:07:22 -0400

    remove sitemap comment

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -4,7 +4,6 @@ import { GetServerSideProps } from 'next';
 function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
      <url>
        <loc>https://example.com</loc>
      </url>
